1af4c09ae4307c3b430d17015dbc2d14c412b0ec
MasterZheng <383889336@qq.com>
2018-03-14 15:18:41 -0400

    change code

--- src/edu/buffalo/www/cse4562/Table/TupleComparator.java
+++ src/edu/buffalo/www/cse4562/Table/TupleComparator.java
@@ -26,27 +26,51 @@ public class TupleComparator implements Comparator<Tuple> {
             try {
                 if (leftValue instanceof LongValue) {
                     if (leftValue.toLong() > rightValue.toLong()) {
-                        return element.isAsc() ? 1 : -1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     } else if (leftValue.toLong() < rightValue.toLong()) {
-                        return element.isAsc() ? -1 : 1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     }
                 } else if (leftValue instanceof DoubleValue) {
                     if (leftValue.toDouble() > rightValue.toDouble()) {
-                        return element.isAsc() ? 1 : -1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     } else if (leftValue.toDouble() < rightValue.toDouble()) {
-                        return element.isAsc() ? -1 : 1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     }
                 } else if (leftValue instanceof StringValue) {
                     if (leftValue.toString().compareTo(rightValue.toString()) > 0) {
-                        return element.isAsc() ? 1 : -1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     } else if (leftValue.toString().compareTo(rightValue.toString()) < 0) {
-                        return element.isAsc() ? -1 : 1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     }
                 } else if (leftValue instanceof DateValue) {
                     if (((DateValue) leftValue).getValue().getTime() > ((DateValue) rightValue).getValue().getTime()) {
-                        return element.isAsc() ? 1 : -1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     } else if (((DateValue) leftValue).getValue().getTime() < ((DateValue) rightValue).getValue().getTime()) {
-                        return element.isAsc() ? -1 : 1;
+                        if (element.isAsc())
+                            return 1;
+                        else
+                            return -1;
                     }
                 }
             } catch (Exception e) {
