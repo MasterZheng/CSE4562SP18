43c3d988cfea7235cad5cc89277a656d7f018458
MasterZheng <383889336@qq.com>
2018-04-09 11:59:47 -0400

    Revert "optimize"
    This reverts commit ee41a65e7b7e84dba82f37a6bca8e1f77cccccef.

--- src/edu/buffalo/www/cse4562/Table/TableObject.java
+++ src/edu/buffalo/www/cse4562/Table/TableObject.java
@@ -22,10 +22,7 @@ public class TableObject {
 
     //when the table is a query result, it is necessary to record the table info about the column
 
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of ee41a65... optimize
     private List<Tuple> tupleList = new ArrayList<>();
     private HashMap<Integer, ArrayList<Tuple>> hashMap = new HashMap<>();
     private List<Integer> mapRelations = new ArrayList<>();
